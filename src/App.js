a187d10ccd5d7314f230ac62d6f4e62e8d3e9003
Nai-Doe <nai.d.doe@gmail.com>
2021-06-22 16:55:13 +0200

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Login from './Login'
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return <Login />
--- src/Login.js
+++ src/Login.js
@@ -5,7 +5,9 @@ const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=9ddbe022c6194
 
 export default function Login() {
     return (
-        <Container>
+        <Container 
+        className='d-flex justify-content-center align-items-center' 
+        style={{ minHeight: "100vh" }}>
             <a className='btn btn-success btn-lg' href={AUTH_URL}>
                 Login with Spotify
             </a>
