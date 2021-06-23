1574afd9dddb7c630fb3223dbabad7f10d6c541b
Nai-Doe <nai.d.doe@gmail.com>
2021-06-23 11:15:13 +0200

    login or dash

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
 import Login from './Login'
+import Dashboard from './Dashboard'
 import "bootstrap/dist/css/bootstrap.min.css"
 
+const code = new URLSearchParams(window.location.search).get('code')
+
 function App() {
-  return <Login />
+  return code ? <Dashboard code={code} /> : <Login />
 }
 
 export default App;
---
+++ src/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Dashboard({ code }) {
+    return (
+        <div>
+            bravo le s
+        </div>
+    )
+}
