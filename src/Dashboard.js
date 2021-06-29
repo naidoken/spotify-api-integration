f4538f7f3c4f16067645f520639fe4e6b2717257
Nai-Doe <nai.d.doe@gmail.com>
2021-06-29 13:48:46 +0200

    end

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,11 +1,90 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import useAuth from './useAuth';
+import { Container, Form } from 'react-bootstrap'
+import SpotifyWebApi from 'spotify-web-api-node';
+import TrackSearchResults from './TrackSearchResults';
+import Player from './Player';
+import axios from 'axios'
+
+
+const spotifyApi = new SpotifyWebApi({
+    clientId: "9ddbe022c6194668b50ab3abd65eb891",
+})
 
 export default function Dashboard({ code }) {
     const accessToken = useAuth(code);
+    const [search, setSearch] = useState('');
+    const [searchResults, setSearchResults] = useState([]);
+    const [playingTrack, setPlayingTrack] = useState();
+    const [lyrics, setLyrics] = useState("");
+
+    function chooseTrack(track) {
+        setPlayingTrack(track)
+        setSearch('')
+        setLyrics('')
+    }
+
+    useEffect(() => {
+        if(!playingTrack) return
+
+        axios.get('http://localhost:3001/lyrics', {
+            params: {
+                track: playingTrack.title,
+                artist: playingTrack.artist
+            }
+        }).then(res => {
+            setLyrics(res.data.lyrics)
+        })
+    }, [playingTrack])
+
+    useEffect(() => {
+        if (!accessToken) return
+        spotifyApi.setAccessToken(accessToken)
+    }, [accessToken]);
+
+    useEffect(() => {
+        if (!search) return setSearchResults([])
+        if (!accessToken) return
+
+        let cancel = false
+        spotifyApi.searchTracks(search).then(res => {
+            if (cancel) return
+            setSearchResults(res.body.tracks.items.map(tracks => {
+
+                const smallestAlbumImage = tracks.album.images.reduce((smallest, image) => {
+                    if (image.height < smallest.height) return image
+                    return smallest
+                }, tracks.album.images[0])
+
+                return {
+                    artist: tracks.artists[0].name,
+                    title: tracks.name,
+                    uri: tracks.uri,
+                    albumUrl: smallestAlbumImage.url
+                }
+            }))
+        })
+
+        return () => cancel = true
+    }, [search, accessToken])
+
     return (
-        <div>
-        {code}
-        </div>
+        <Container className="d-flex flex-column py-2" style={{height:'100vh'}}>
+            <Form.Control
+                type="search" 
+                placeholder="Search Song/Artist" 
+                value={search} 
+                onChange={e => {setSearch(e.target.value)}} />
+                <div className="flex-grow-1 my-2" style={{ overflowY: "auto"}}>
+                    {searchResults.map(track => (
+                        <TrackSearchResults track={track} key={track.uri} chooseTrack={chooseTrack}/>
+                    ))}
+                    {searchResults.length === 0 && (
+                        <div className="text-center" style={{ whiteSpace: "pre"}}>{lyrics}</div>
+                    )}
+                </div>
+                <div><Player accessToken={accessToken} trackUri={playingTrack?.uri}/></div>
+        </Container>
     )
 }
